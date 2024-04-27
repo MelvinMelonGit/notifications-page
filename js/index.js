1ad3f83b6b37ca58e71f14907ac33b4562c387cc
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2024-04-27 21:22:07 +0800

    Updated js for notification count

--- js/index.js
+++ js/index.js
@@ -2,14 +2,24 @@ const notificationSingles = [...document.querySelectorAll(".notification-single"
 
 const markRead = document.querySelector(".mark-read")
 
+const notificationSpan = document.querySelector(".notification-heading span")
+
+let notificationCount = parseInt(notificationSpan.innerText) || 0
+
 notificationSingles.forEach(notification => {
     notification.addEventListener("click", () => {
         notification.classList.toggle("unread")
-    })
+
+        notification.classList.contains("unread") ? notificationCount++ : notificationCount--
+        notificationSpan.innerText = notificationCount
+    }) 
 })
 
 markRead.addEventListener("click", () => {
     notificationSingles.forEach(notification => {
         notification.classList.remove("unread")
     })
+
+    notificationCount = 0
+    notificationSpan.innerText = notificationCount
 })
